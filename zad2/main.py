842eebf402ada3b663c07391cfa34f3c88a6befa
kapis18 <kapis1881@gmail.com>
2020-10-27 14:43:00 +0100

    Implemented field with information if string contains special chars

--- zad2/main.py
+++ zad2/main.py
@@ -15,12 +15,17 @@ def string_has_number(query):
     return True if any(letter.isdigit() for letter in query) else False
 
 
+def string_has_special_chars(query):
+    return True if any(not letter.isalnum() for letter in query) else False
+
+
 @app.route('/string/<query>', methods=['GET'])
 def rev(query):
     result = {
         "hasUpper": string_has_uppercase(query),
         "hasLower": string_has_lowercase(query),
-        "hasNumber": string_has_number(query)
+        "hasNumber": string_has_number(query),
+        "hasSpecial": string_has_special_chars(query)
     }
     return result
 
