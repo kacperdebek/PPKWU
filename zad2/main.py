081be2a6b9f63083d432a9c1f60de1233e2c4204
kapis18 <kapis1881@gmail.com>
2020-10-27 14:27:35 +0100

    Added function for checking if string has any uppercase letter

--- zad2/main.py
+++ zad2/main.py
@@ -3,6 +3,10 @@ import flask
 app = flask.Flask(__name__)
 
 
+def string_has_uppercase(query):
+    return True if any(letter.isupper() for letter in query) else False
+
+
 @app.route('/string/<query>', methods=['GET'])
 def rev(query):
     return f"Success! Parameter: {query}"
