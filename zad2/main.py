e071baa4a73885a001f04a65d246607c43ae8a31
kapis18 <kapis1881@gmail.com>
2020-10-27 14:28:45 +0100

    Added function for checking if string has any lowercase letters

--- zad2/main.py
+++ zad2/main.py
@@ -7,6 +7,10 @@ def string_has_uppercase(query):
     return True if any(letter.isupper() for letter in query) else False
 
 
+def string_has_lowercase(query):
+    return True if any(letter.islower() for letter in query) else False
+
+
 @app.route('/string/<query>', methods=['GET'])
 def rev(query):
     return f"Success! Parameter: {query}"
