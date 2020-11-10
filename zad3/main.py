a7bc890ec08872739716e8d54c747d1e65d478a9
kapis18 <kapis1881@gmail.com>
2020-11-10 15:40:09 +0100

    switched dict to list and added the event names

--- zad3/main.py
+++ zad3/main.py
@@ -1,12 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+from icalendar import Calendar, Event
+from datetime import datetime
+from pytz import UTC  # timezone
 
 url = 'http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, features="html.parser")
-
+print(r.text)
 calendar_days = soup.find_all('a', href=True)[2:]
+event_names = soup.find_all('div', {"class": "InnerBox"})
+for elem in event_names:
+    print(elem.text)
 
-calendar_dict = {elem.text: elem.get('href') for elem in calendar_days}
+calendar_dict = [[elem.text, elem.get('href')] for elem in calendar_days]
+for elem in calendar_dict:
+    if elem[1] == "javascript:void();":
+        elem.append("")
+    else:
+        elem.append(event_names.pop(0).text)
 
 print(calendar_dict)
+
+
+
