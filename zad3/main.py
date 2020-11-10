ae16d88490f51b87a9a5f951629535bbb55d9688
kapis18 <kapis1881@gmail.com>
2020-11-10 15:16:39 +0100

    created a dictionary mapping days of the month to events

--- zad3/main.py
+++ zad3/main.py
@@ -3,7 +3,10 @@ from bs4 import BeautifulSoup
 
 url = 'http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1'
 r = requests.get(url)
-print(r.text)
 soup = BeautifulSoup(r.text, features="html.parser")
+
 calendar_days = soup.find_all('a', href=True)[2:]
-print(calendar_days)
+
+calendar_dict = {elem.text: elem.get('href') for elem in calendar_days}
+
+print(calendar_dict)
