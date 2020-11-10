cabd6e05c6129864e7c8c65db19b140833f06d61
kapis18 <kapis1881@gmail.com>
2020-11-10 15:56:39 +0100

    added a function to create calendar using parsed information

--- zad3/main.py
+++ zad3/main.py
@@ -4,6 +4,23 @@ from icalendar import Calendar, Event
 from datetime import datetime
 from pytz import UTC  # timezone
 
+
+def create_calendar(l):
+    cal = Calendar()
+
+    for ev in l:
+        event = Event()
+        event.add('summary', ev[2])
+        event.add('dtstart', datetime(2020, 11, int(ev[0]), 0, 0, 0, tzinfo=UTC))
+        event.add('dtend', datetime(2020, 11, int(ev[0]), 23, 0, 0, tzinfo=UTC))
+        event.add('description', ev[1])
+        cal.add_component(event)
+
+    f = open('calendar.ics', 'wb')
+    f.write(cal.to_ical())
+    f.close()
+
+
 url = 'http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, features="html.parser")
@@ -17,6 +34,4 @@ calendar_dict = [[elem.text, elem.get('href')] for elem in calendar_days if elem
 for elem in calendar_dict:
     elem.append(event_names.pop(0).text)
 
-print(calendar_dict)
-
-
+create_calendar(calendar_dict)
