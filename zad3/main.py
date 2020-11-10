da472f3d1df707a7bed0351dff9b595ededf4dc0
kapis18 <kapis1881@gmail.com>
2020-11-10 14:43:15 +0100

    fetching html calendar page from the weeia homepage

--- zad3/main.py
+++ zad3/main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+url = 'http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1'
+r = requests.get(url)
+print(r.text)
