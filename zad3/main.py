cc72db5f9ef34033f9f5169bd9d9c20d77d4f160
kapis18 <kapis1881@gmail.com>
2020-11-10 15:01:19 +0100

    importing and experimenting with beautifulsoup html parsing

--- zad3/main.py
+++ zad3/main.py
@@ -1,5 +1,7 @@
 import requests
-
+from bs4 import BeautifulSoup
 url = 'http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1'
 r = requests.get(url)
 print(r.text)
+soup = BeautifulSoup(r.text, features="html.parser")
+print(soup.find_all('a', href=True))
