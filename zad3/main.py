8c9c487352a626a56b6d42c0e487ab296345ac70
kapis18 <kapis1881@gmail.com>
2020-11-10 15:07:48 +0100

    fetching all calendar days htmls into a python list

--- zad3/main.py
+++ zad3/main.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+
 url = 'http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1'
 r = requests.get(url)
 print(r.text)
 soup = BeautifulSoup(r.text, features="html.parser")
-print(soup.find_all('a', href=True))
+calendar_days = soup.find_all('a', href=True)[2:]
+print(calendar_days)
