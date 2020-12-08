d3a77d979915fa4d6f9cb787a4c4db356b1b0407
kapis18 <kapis1881@gmail.com>
2020-12-08 15:04:30 +0100

    Packed the scraper into a function with passable query

--- zad4/main.py
+++ zad4/main.py
@@ -1,16 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://panoramafirm.pl/szukaj?k=hydraulik&l='
-r = requests.get(url)
-soup = BeautifulSoup(r.text, features="html.parser")
 
-addresses = soup.findAll('div', {"class": "address"})
-phone_numbers = soup.findAll('a', {"class": "icon-telephone"})
-emails_and_names = soup.findAll('a', {"class": "ajax-modal-link"})
+def scrape_data(query):
+    url = f'https://panoramafirm.pl/szukaj?k={query}&l='
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, features="html.parser")
 
-company_data = [{'name': name.get('data-company-name'),
-                 'email': name.get('data-company-email'),
-                 'phone': phone.get('title'),
-                 'address': address.text.strip()} for name, phone, address in zip(emails_and_names, phone_numbers, addresses)]
-print(company_data)
+    addresses = soup.findAll('div', {"class": "address"})
+    phone_numbers = soup.findAll('a', {"class": "icon-telephone"})
+    emails_and_names = soup.findAll('a', {"class": "ajax-modal-link"})
+
+    company_data = [{'name': name.get('data-company-name'),
+                     'email': name.get('data-company-email'),
+                     'phone': phone.get('title'),
+                     'address': address.text.strip()} for name, phone, address in
+                    zip(emails_and_names, phone_numbers, addresses)]
+    return company_data
+
